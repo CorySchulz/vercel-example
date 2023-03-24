27082d5245119e6d39716a823ac037de0e1c156b
CorySchulz <coryschulz@gmail.com>
2023-03-24 18:37:04 -0500

    test

---
+++ public/api/users.js
@@ -0,0 +1,8 @@
+
+// Fake users data
+const users = [{ id: 1, name: "Cory" }, { id: 2 }, { id: 3 }]
+
+export default function handler(req, res) {
+  // Get data from your database
+  res.status(200).json(users)
+}
